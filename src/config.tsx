62f0ba2f46ce6757928470fadae2d5d0c532359d
tarun-strrd <tarunsayyapureddi123@gmail.com>
2023-08-25 00:50:06 +0530

    added dev and prod base urls

--- src/config.tsx
+++ src/config.tsx
@@ -1,5 +1,7 @@
-//const baseUrl: string = "https://jittery-lime-woodpecker.cyclic.cloud";
-const baseUrl: string = "http://localhost:5000";
+const prodBaseUrl: string = "https://jittery-lime-woodpecker.cyclic.cloud";
+const devBaseUrl: string = "http://localhost:5000";
 const cloudinaryUrl: string =
   "https://api.cloudinary.com/v1_1/dfqau4u7l/image/upload";
+const baseUrl: string =
+  process.env.NODE_ENV === "production" ? prodBaseUrl : devBaseUrl;
 export { baseUrl, cloudinaryUrl };
